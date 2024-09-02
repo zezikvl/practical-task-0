53e99567c2816c8a6b5f452bb5908ee33fc8f126
zezikvl <zezik.vl@gmail.com>
2024-09-02 18:40:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#1st program
+print(9 ** 0.5 * 5)
+
+#2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3st program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+#4st program
+print((int(float('123.456') * 10)) % 10)
